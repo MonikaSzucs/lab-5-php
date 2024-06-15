12c12004226c076ad7356f8ac893ddfa0b5858d7
Monika Szucs <monika.szucs.work@gmail.com>
2024-06-14 20:05:39 -0700

    saving updated code so far

--- tests/PostRepositoryTest.php
+++ tests/PostRepositoryTest.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 
 use src\Repositories\PostRepository;
+use src\Models\Post;
 
 class PostRepositoryTest extends TestCase {
 
@@ -36,16 +37,59 @@ class PostRepositoryTest extends TestCase {
 		$this->assertEquals('body', $post->body);
 	}
 
+	// Starting here and below
 	public function testPostRetrieval() {
 		// TODO test the "get" methods in the PostRepository class
-		//$this->assertTrue(true);
+		// Create a new post to retrieve
+		$createdPost = $this->postRepository->savePost('retrieve title', 'retrieve body');
+		
+		// Retrieve the post by ID
+		$retrievedPost = $this->postRepository->getPostById($createdPost->id);
+		
+		// Assert the retrieved post matches the created post
+		$this->assertInstanceOf(Post::class, $retrievedPost); // checks if we did get a post, aka not a false
+		$this->assertNotEmpty($createdPost->title);
+		$this->assertNotEmpty($createdPost->body);
+		$this->assertEquals($createdPost->id, $retrievedPost->id);
+		$this->assertEquals($createdPost->title, $retrievedPost->title);
+		$this->assertEquals($createdPost->body, $retrievedPost->body);
+		
+		// Retrieve all posts and check the created post is in the list
+		$allPosts = $this->postRepository->getAllPosts();
+		$this->assertIsArray($allPosts);
+		$this->assertNotEmpty($allPosts);
+		$this->assertContainsOnlyInstancesOf(Post::class, $allPosts);
+		$this->assertContains($createdPost, $allPosts);
 	}
 
 	public function testPostUpdate() {
 		// TODO create a post, update the title and body, and check that you get the expected title and body
+		// Create a new post to update
+		$createdPost = $this->postRepository->savePost('original title', 'original body');
+		
+		// Update the post
+		$updated = $this->postRepository->updatePost($createdPost->id, 'updated title', 'updated body');
+		$this->assertTrue($updated);
+		
+		// Retrieve the updated post
+		$updatedPost = $this->postRepository->getPostById($createdPost->id);
+		
+		// Assert the post was updated
+		$this->assertInstanceOf(Post::class, $updatedPost);
+		$this->assertEquals('updated title', $updatedPost->title);
+		$this->assertEquals('updated body', $updatedPost->body);
 	}
 
 	public function testPostDeletion() {
 		// TODO: delete a post by ID and check that it isn't in the database anymore
+		$createdPost = $this->postRepository->savePost('delete title', 'delete body');
+		
+		// Delete the post
+		$deleted = $this->postRepository->deletePostById($createdPost->id);
+		$this->assertTrue($deleted);
+		
+		// Assert the post is no longer in the database
+		$retrievedPost = $this->postRepository->getPostById($createdPost->id);
+		$this->assertFalse($retrievedPost);
 	}
 }
